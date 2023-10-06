e4af297ae607f8b4ca7703fdab989c56dca419fb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-06 16:07:37 +0300

    Created the NavBar component

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
+import { BsFillPersonFill } from "react-icons/bs";
 
 type Props = {};
 
 const NavBar = (props: Props) => {
   return (
-    <nav className="flex justify-between items-center p-4 bg-white h-[5rem]">
-      <div className="">
+    <nav className="flex justify-between items-center p-[1rem] bg-zinc-100 h-[5rem] text-black shadow-md sticky">
+      <div className="rounded-full">
         <Image
           src="/images/logo.jpg"
           alt="Company Logo"
@@ -14,8 +16,15 @@ const NavBar = (props: Props) => {
           height={100}
         />
       </div>
-      <div className=""></div>
-      <div className=""></div>
+      <div className="">
+        <h1 className="font-bold text-xl uppercase">Second Hand Cars</h1>
+      </div>
+      <div>
+        <Link href={"/signin"} className="flex flex-row gap-2 items-center">
+          <BsFillPersonFill className="text-xl" />
+          <p className="text-bold">Sign In</p>
+        </Link>
+      </div>
     </nav>
   );
 };
