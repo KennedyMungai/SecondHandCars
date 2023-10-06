a106ecd1256a49fada158146fbff3d03c97c5ebc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-06 11:40:09 +0300

    Added a startup event to call the function that initializes communication with the database

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,19 @@
 """The main python file for the backend"""
 from fastapi import FastAPI
+from database.db import init_db
 
 
 app = FastAPI(
-    title="Second Hand Cars", 
-    description="The backend of a second hand cars app", 
+    title="Second Hand Cars",
+    description="The backend of a second hand cars app",
     version="0.1.0"
 )
+
+
+@app.on_event("startup")
+async def initialize_database():
+    """Initializing the application connection to the database"""
+    await init_db()
 
 
 @app.get("/")
