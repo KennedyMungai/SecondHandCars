0569f7da8a81c1a4b5defc4ac6ea1eee2ed90a8d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-06 16:19:49 +0300

    Made some changes

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,8 +2,7 @@ export default function Home() {
   return (
     <main className="bg-zinc-800 h-[90vh]">
       <h1 className="text-zinc-100 text-l font-bold text-center uppercase flex items-center w-full h-full justify-center">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam,
-        exercitationem?
+        Welcome to Second Hand Cars
       </h1>
     </main>
   );
