7543c86168a3f88d0cc6a167ef25b9faf701e7e4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-06 15:12:24 +0300

    Fixed some bugs for the login endpoint

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -10,15 +10,15 @@ auth_router = APIRouter(prefix='/auth', tags=["Auth"])
 
 @auth_router.post('/login')
 async def user_login(user_data: OAuth2PasswordRequestForm = Depends()):
-    db_user = await Users.find_one({"username": user_data.username})
+    db_user = await Users.find_one({"email": user_data.username})
 
-    if not db_user or verify_password(user_data.password, db_user.password_hash):
+    if not db_user or verify_password(user_data.password, db_user.password_hash) == False:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Invalid Credentials"
         )
 
-    access_token = create_access_token(data={"sub": db_user._id})
+    access_token = create_access_token(data={"sub": db_user.email})
 
     return {"access_token": access_token, "token_type": "bearer"}
 
