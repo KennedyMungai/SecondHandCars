60789d4550fc5ca283dcdb3fe5db239cbdd3f508
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-06 15:12:38 +0300

    Some simple data type conversion

--- backend/utils/oauth2.py
+++ backend/utils/oauth2.py
@@ -26,7 +26,7 @@ def create_access_token(data: dict) -> str:
     """
     to_encode = data.copy()
 
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
     to_encode.update({"exp": expire})
 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
