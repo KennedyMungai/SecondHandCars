2b82b06c2bb3f246faf96fd8d463e1771233aa00
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-06 14:05:44 +0300

    Added the users Document

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,10 +1,10 @@
 """The config python file for the databases"""
 from beanie import init_beanie
-from models.models import Cars
+from models.models import Cars, Users
 from motor.motor_asyncio import AsyncIOMotorClient
 
 
 async def init_db():
     """The function needed to initialize the database connection"""
     client = AsyncIOMotorClient("mongodb://localhost:27017")
-    await init_beanie(database=client.Cars, document_models=[Cars])
+    await init_beanie(database=client.Cars, document_models=[Cars, Users])
