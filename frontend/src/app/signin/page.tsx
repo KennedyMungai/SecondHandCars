ebc55bedc81a2044fe98727a0465691f6acce3bd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-07 08:48:23 +0300

    Added some styling

--- frontend/src/app/signin/page.tsx
+++ frontend/src/app/signin/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 type Props = {};
 
 const SignInPage = (props: Props) => {
-  return <div>This is the sign in page</div>;
+  return <main className="h-[90vh] bg-zinc-800">This is the sign in page</main>;
 };
 
 export default SignInPage;
