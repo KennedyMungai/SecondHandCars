8d121fbb4a88a020df8ead7fe83ba59e24e67257
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-07 08:48:47 +0300

    The signup page for the application

---
+++ frontend/src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const SignUpPage = (props: Props) => {
+  return <main className="h-[90vh] bg-zinc-800">This is the sign up page</main>;
+};
+
+export default SignUpPage;
